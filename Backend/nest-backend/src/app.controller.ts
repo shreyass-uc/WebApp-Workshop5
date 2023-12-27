e93c4c894c9d340e45f376346930ec2f20f66308
shreyas <shreyass@unicourt.com>
2023-12-27 17:34:28 +0530

    env sync

--- Backend/nest-backend/src/app.controller.ts
+++ Backend/nest-backend/src/app.controller.ts
@@ -6,9 +6,6 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { CsvService } from './bull/job.add.service';
-import multer from 'multer';
-import { diskStorage } from 'multer';
-import { extname } from 'path';
 @Controller('csv')
 export class AppController {
   constructor(private readonly csvService: CsvService) {}
